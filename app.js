3a83e664b0ea1171033ec442618444ffd23f6579
FabianMedel <rubenafm1417outlook.es>
2021-04-14 23:34:09 -0500

    create endpoints structure

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const router = require('./src/routes');
 
 const {json, urlencoded} = express;
-
 const app = express();
 
-const port = process.env.PORT || 8080;
+app.use(json());
+app.use(urlencoded({ extended: false}));
 
 const corsOptions = {
     origin: '*',
@@ -14,10 +15,13 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
+app.use(router);
 
 app.use('/', (req,res) => {
-    res.send("this is the microservicio v.1.0.0");
+    res.send("This is the microservicio v.1.0.0");
 });
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () =>{
     console.log(`Server listening on port ${port}`)
